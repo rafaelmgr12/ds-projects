aa03581be8b59f2ef937ae655f4abe490e8541fc
Rafael <ribeirorafaelmatheus@gmail.com>
2020-11-16 16:43:17 -0300

    fixes

--- functions/ml_functions.py
+++ functions/ml_functions.py
@@ -108,7 +108,7 @@ class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
 
 
 # Validation function
-def rmsle_cv(model):
+def rmsle_cv(model,n_folds):
     '''Return the rmse form a model'''
     kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(train.values)
     rmse= np.sqrt(-cross_val_score(model, train.values, y_train, scoring="neg_mean_squared_error", cv = kf))
