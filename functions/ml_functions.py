cad8477037454eb80b84025b885b1d88c8f2ed56
Rafael <ribeirorafaelmatheus@gmail.com>
2020-11-22 15:30:02 -0300

    new project and functios

--- functions/ml_functions.py
+++ functions/ml_functions.py
@@ -9,6 +9,14 @@ from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.metrics import mean_squared_error
 import xgboost as xgb
+from sklearn.model_selection import KFold, StratifiedKFold
+
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, classification_report
+
+
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import learning_curve
+import matplotlib.patches as mpatches
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.preprocessing import StandardScaler
@@ -23,16 +31,18 @@ from xgboost import XGBRegressor
 from sklearn.svm import SVR
 from sklearn.linear_model import Ridge, Lasso
 
+import matplotlib.pyplot as plt
 
-def train_models(X, y, kfolds):
+
+def train_models(X, y, kfolds, models):
     kfold = KFold(kfolds)
     for model in models:
      # model['regressor'].fit(X,y)
-      cf_result = cross_val_score(
-          model['regressor'], X, y, cv=kfold, scoring='neg_mean_squared_log_error')
-      model['cv_results'] = np.sqrt(cf_result*-1)
-      msg = f"Regressor: {model['name']},   rmsle:{cf_result.mean().round(11)} "
-      print(msg)
+        cf_result = cross_val_score(
+            model['regressor'], X, y, cv=kfold, scoring='neg_mean_squared_log_error')
+        model['cv_results'] = np.sqrt(cf_result*-1)
+        msg = f"Regressor: {model['name']},   rmsle:{cf_result.mean().round(11)} "
+        print(msg)
 
 
 class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
@@ -70,17 +80,18 @@ class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
         inputs, and the correct responses (target variable) as the outputs to
         train a higher level learner called meta-model.
     '''
+
     def __init__(self, base_models, meta_model, n_folds):
         self.base_models = base_models
         self.meta_model = meta_model
         self.n_folds = n_folds
-   
+
     # We again fit the data on clones of the original models
     def fit(self, X, y):
         self.base_models_ = [list() for x in self.base_models]
         self.meta_model_ = clone(self.meta_model)
         kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
-        
+
         # Train cloned base models then create out-of-fold predictions
         # that are needed to train the cloned meta-model
         out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
@@ -91,25 +102,142 @@ class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
                 instance.fit(X[train_index], y[train_index])
                 y_pred = instance.predict(X[holdout_index])
                 out_of_fold_predictions[holdout_index, i] = y_pred
-                
+
         # Now train the cloned  meta-model using the out-of-fold predictions as new feature
         self.meta_model_.fit(out_of_fold_predictions, y)
         return self
-   
-    # Do the predictions of all base models on the test data and use the averaged predictions as 
+
+    # Do the predictions of all base models on the test data and use the averaged predictions as
     # meta-features for the final prediction which is done by the meta-model
     def predict(self, X):
         meta_features = np.column_stack([
-            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
-            for base_models in self.base_models_ ])
+            np.column_stack([model.predict(X)
+                             for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_])
         return self.meta_model_.predict(meta_features)
 
 
+def sssplit(X, y, nsplits):
+    '''This functions it to lead with unbaleced data set, and 
+    make new balanced splits into train and test .
+    '''
+    sss = StratifiedKFold(n_splits=nsplits, random_state=None, shuffle=False)
+
+    for train_index, test_index in sss.split(X, y):
+        print("Train:", train_index, "Test:", test_index)
+        original_Xtrain, original_Xtest = X.iloc[train_index], X.iloc[test_index]
+        original_ytrain, original_ytest = y.iloc[train_index], y.iloc[test_index]
+
+    return original_Xtrain, original_Xtest, original_ytrain, original_ytest
 
 
-# Validation function
-def rmsle_cv(model,n_folds):
-    '''Return the rmse form a model'''
-    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(train.values)
-    rmse= np.sqrt(-cross_val_score(model, train.values, y_train, scoring="neg_mean_squared_error", cv = kf))
-    return(rmse)
+def plot_learning_curve(estimator1, estimator2, estimator3, estimator4, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, figsize=(20,14), sharey=True)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    # First Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator1, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax1.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax1.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax1.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax1.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax1.set_title("Logistic Regression Learning Curve", fontsize=14)
+    ax1.set_xlabel('Training size (m)')
+    ax1.set_ylabel('Score')
+    ax1.grid(True)
+    ax1.legend(loc="best")
+    
+    # Second Estimator 
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator2, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax2.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax2.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax2.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax2.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax2.set_title("Knears Neighbors Learning Curve", fontsize=14)
+    ax2.set_xlabel('Training size (m)')
+    ax2.set_ylabel('Score')
+    ax2.grid(True)
+    ax2.legend(loc="best")
+    
+    # Third Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator3, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax3.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax3.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax3.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax3.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax3.set_title("Support Vector Classifier \n Learning Curve", fontsize=14)
+    ax3.set_xlabel('Training size (m)')
+    ax3.set_ylabel('Score')
+    ax3.grid(True)
+    ax3.legend(loc="best")
+    
+    # Fourth Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator4, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax4.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax4.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax4.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax4.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax4.set_title("Decision Tree Classifier \n Learning Curve", fontsize=14)
+    ax4.set_xlabel('Training size (m)')
+    ax4.set_ylabel('Score')
+    ax4.grid(True)
+    ax4.legend(loc="best")
+    return plt
+
+
+def graph_roc_curve_multiple(log_fpr, log_tpr, knear_fpr, knear_tpr, svc_fpr, svc_tpr, tree_fpr, tree_tpr):
+    plt.figure(figsize=(16,8))
+    plt.title('ROC Curve \n Top 4 Classifiers', fontsize=18)
+    plt.plot(log_fpr, log_tpr, label='Logistic Regression Classifier Score: {:.4f}'.format(roc_auc_score(y_train, log_reg_pred)))
+    plt.plot(knear_fpr, knear_tpr, label='KNears Neighbors Classifier Score: {:.4f}'.format(roc_auc_score(y_train, knears_pred)))
+    plt.plot(svc_fpr, svc_tpr, label='Support Vector Classifier Score: {:.4f}'.format(roc_auc_score(y_train, svc_pred)))
+    plt.plot(tree_fpr, tree_tpr, label='Decision Tree Classifier Score: {:.4f}'.format(roc_auc_score(y_train, tree_pred)))
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([-0.01, 1, 0, 1])
+    plt.xlabel('False Positive Rate', fontsize=16)
+    plt.ylabel('True Positive Rate', fontsize=16)
+    plt.annotate('Minimum ROC Score of 50% \n (This is the minimum score to get)', xy=(0.5, 0.5), xytext=(0.6, 0.3),
+                arrowprops=dict(facecolor='#6E726D', shrink=0.05),
+                )
+    plt.legend()
