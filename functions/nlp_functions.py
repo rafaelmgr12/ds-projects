e28a1ddfc17c717a732836ed99e6e6dfb25af99f
Rafael <ribeirorafaelmatheus@gmail.com>
2021-06-09 12:26:21 -0300

    New functions

---
+++ functions/nlp_functions.py
@@ -0,0 +1,125 @@
+import nltk
+import re
+import numpy as np
+from unidecode import unidecode
+from nltk.tokenize import word_tokenize
+from nltk import SnowballStemmer
+import pandas as pd
+
+# removes a list of words (ie. stopwords) from a tokenized list.
+
+
+def removeWords(listOfTokens, listOfWords):
+    return [token for token in listOfTokens if token not in listOfWords]
+
+# applies stemming to a list of tokenized words
+
+
+def applyStemming(listOfTokens, stemmer):
+    return [stemmer.stem(token) for token in listOfTokens]
+
+# removes any words composed of less than 2 or more than 21 letters
+
+
+def twoLetters(listOfTokens):
+    twoLetterWord = []
+    for token in listOfTokens:
+        if len(token) <= 2 or len(token) >= 21:
+            twoLetterWord.append(token)
+    return twoLetterWord
+
+
+def is_noun(pos): return pos[:2] == 'NN'
+
+
+def keywords_inventory(dataframe, colonne='Description'):
+    stemmer = nltk.stem.SnowballStemmer("english")
+    keywords_roots = dict()  # collect the words / root
+    keywords_select = dict()  # association: root <-> keyword
+    category_keys = []
+    count_keywords = dict()
+    icount = 0
+    for s in dataframe[colonne]:
+        if pd.isnull(s):
+            continue
+        lines = s.lower()
+        tokenized = nltk.word_tokenize(lines)
+        nouns = [word for (word, pos) in nltk.pos_tag(
+            tokenized) if is_noun(pos)]
+
+        for t in nouns:
+            t = t.lower()
+            racine = stemmer.stem(t)
+            if racine in keywords_roots:
+                keywords_roots[racine].add(t)
+                count_keywords[racine] += 1
+            else:
+                keywords_roots[racine] = {t}
+                count_keywords[racine] = 1
+
+    for s in keywords_roots.keys():
+        if len(keywords_roots[s]) > 1:
+            min_length = 1000
+            for k in keywords_roots[s]:
+                if len(k) < min_length:
+                    clef = k
+                    min_length = len(k)
+            category_keys.append(clef)
+            keywords_select[s] = clef
+        else:
+            category_keys.append(list(keywords_roots[s])[0])
+            keywords_select[s] = list(keywords_roots[s])[0]
+
+    print("Nb of keywords in variable '{}': {}".format(
+        colonne, len(category_keys)))
+    return category_keys, keywords_roots, keywords_select, count_keywords
+
+
+def processCorpus(corpus, language):
+    stopwords = nltk.corpus.stopwords.words(language)
+    param_stemmer = SnowballStemmer(language)
+    countries_list = [line.rstrip('\n') for line in open(
+        '/home/rafael/Projetos/ds-projects/functions/lists/countries.txt')]  # Load .txt file line by line
+    nationalities_list = [line.rstrip('\n') for line in open(
+        '/home/rafael/Projetos/ds-projects/functions/lists/nationalities.txt')]  # Load .txt file line by line
+    other_words = [line.rstrip('\n') for line in open(
+        '/home/rafael/Projetos/ds-projects/functions/lists/stopwords_scrapmaker.txt')]  # Load .txt file line by line
+
+    for document in corpus:
+        index = corpus.index(document)
+        # Replaces the ASCII '�' symbol with '8'
+        corpus[index] = corpus[index].replace(u'\ufffd', '8')
+        corpus[index] = corpus[index].replace(
+            ',', '')          # Removes commas
+        corpus[index] = corpus[index].rstrip(
+            '\n')              # Removes line breaks
+        # Makes all letters lowercase
+        corpus[index] = corpus[index].casefold()
+
+        # removes specials characters and leaves only words
+        corpus[index] = re.sub('\W_', ' ', corpus[index])
+        # removes numbers and words concatenated with numbers IE h4ck3r. Removes road names such as BR-381.
+        corpus[index] = re.sub("\S*\d\S*", " ", corpus[index])
+        # removes emails and mentions (words with @)
+        corpus[index] = re.sub("\S*@\S*\s?", " ", corpus[index])
+        # removes URLs with http
+        corpus[index] = re.sub(r'http\S+', '', corpus[index])
+        # removes URLs with www
+        corpus[index] = re.sub(r'www\S+', '', corpus[index])
+
+        listOfTokens = word_tokenize(corpus[index])
+        twoLetterWord = twoLetters(listOfTokens)
+
+        listOfTokens = removeWords(listOfTokens, stopwords)
+        listOfTokens = removeWords(listOfTokens, twoLetterWord)
+        listOfTokens = removeWords(listOfTokens, countries_list)
+        listOfTokens = removeWords(listOfTokens, nationalities_list)
+        listOfTokens = removeWords(listOfTokens, other_words)
+
+        listOfTokens = applyStemming(listOfTokens, param_stemmer)
+        listOfTokens = removeWords(listOfTokens, other_words)
+
+        corpus[index] = " ".join(listOfTokens)
+        corpus[index] = unidecode(corpus[index])
+
+    return corpus
---
+++ functions/plots_functions.py
@@ -0,0 +1,451 @@
+import matplotlib as mpl
+from scipy.cluster.hierarchy import dendrogram
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import seaborn as sns
+from sklearn.metrics import silhouette_samples, silhouette_score
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.patches as mpatches
+import numpy as np
+import cv2
+
+def display_multiple_img(images_paths, rows, cols):
+    """
+    Function to Display Images from Dataset.
+    
+    parameters: images_path(string) - Paths of Images to be displayed
+                rows(int) - No. of Rows in Output
+                cols(int) - No. of Columns in Output
+    """
+    figure, ax = plt.subplots(nrows=rows,ncols=cols,figsize=(16,8) )
+    for ind,image_path in enumerate(images_paths):
+        image=cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+        try:
+            ax.ravel()[ind].imshow(image)
+            ax.ravel()[ind].set_axis_off()
+        except:
+            continue;
+    plt.tight_layout()
+    plt.show()
+
+def plotSilhouette(df, n_clusters, kmeans_labels, silhouette_avg):
+    fig, ax1 = plt.subplots(1)
+    fig.set_size_inches(8, 6)
+    ax1.set_xlim([-0.2, 1])
+    ax1.set_ylim([0, len(df) + (n_clusters + 1) * 10])
+
+    # The vertical line for average silhouette score of all the values
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.2, 0, 0.2, 0.4, 0.6, 0.8, 1])
+    plt.title(("Análise de Silhouette para K = %d" %
+              n_clusters), fontsize=10, fontweight='bold')
+
+    y_lower = 10
+    # Compute the silhouette scores for each sample
+    sample_silhouette_values = silhouette_samples(df, kmeans_labels)
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = sample_silhouette_values[kmeans_labels == i]
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+        y_lower = y_upper + 10  # Compute the new y_lower for next plot. 10 for the 0 samples
+    plt.show()
+
+
+def silhouette(kmeans_dict, df, plot=False):
+    df = df.to_numpy()
+    avg_dict = dict()
+    for n_clusters, kmeans in kmeans_dict.items():
+        kmeans_labels = kmeans.predict(df)
+        # Average Score for all Samples
+        silhouette_avg = silhouette_score(df, kmeans_labels)
+        avg_dict.update({silhouette_avg: n_clusters})
+
+        if(plot):
+            plotSilhouette(df, n_clusters, kmeans_labels, silhouette_avg)
+
+
+def get_top_features_cluster(tf_idf_array, prediction, n_feats):
+    labels = np.unique(prediction)
+    dfs = []
+    for label in labels:
+        id_temp = np.where(prediction == label)  # indices for each cluster
+        # returns average score across cluster
+        x_means = np.mean(tf_idf_array[id_temp], axis=0)
+        # indices with top 20 scores
+        sorted_means = np.argsort(x_means)[::-1][:n_feats]
+        features = vectorizer.get_feature_names()
+        best_features = [(features[i], x_means[i]) for i in sorted_means]
+        df = pd.DataFrame(best_features, columns=['features', 'score'])
+        dfs.append(df)
+    return dfs
+
+
+def plotWords(dfs, n_feats):
+    plt.figure(figsize=(8, 4))
+    for i in range(0, len(dfs)):
+        plt.title(("Palvras mais comunus no Cluster {}".format(i)),
+                  fontsize=10, fontweight='bold')
+        plt.grid()
+        sns.barplot(x='score', y='features', orient='h', data=dfs[i][:n_feats])
+        plt.show()
+# Transforms a centroids dataframe into a dictionary to be used on a WordCloud.
+
+
+def centroidsDict(centroids, index):
+    a = centroids.T[index].sort_values(ascending=False).reset_index().values
+    centroid_dict = dict()
+
+    for i in range(0, len(a)):
+        centroid_dict.update({a[i, 0]: a[i, 1]})
+
+    return centroid_dict
+
+
+def generateWordClouds(centroids):
+    wordcloud = WordCloud(max_font_size=100, background_color='white')
+    for i in range(0, len(centroids)):
+        centroid_dict = centroidsDict(centroids, i)
+        wordcloud.generate_from_frequencies(centroid_dict)
+
+        plt.figure(figsize=(12, 6))
+        plt.title('Cluster {}'.format(i))
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.show()
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    # _________________________________________________
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=0)
+    plt.yticks(tick_marks, classes)
+    # _________________________________________________
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    # _________________________________________________
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+def _scale_data(data, ranges):
+    (x1, x2) = ranges[0]
+    d = data[0]
+    return [(d - y1) / (y2 - y1) * (x2 - x1) + x1 for d, (y1, y2) in zip(data, ranges)]
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 10)):
+    """Generate a simple plot of the test and training learning curve"""
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-',
+             color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-',
+             color="g", label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+class RadarChart():
+    def __init__(self, fig, location, sizes, variables, ranges, n_ordinate_levels=6):
+
+        angles = np.arange(0, 360, 360./len(variables))
+
+        ix, iy = location[:]
+        size_x, size_y = sizes[:]
+
+        axes = [fig.add_axes([ix, iy, size_x, size_y], polar=True,
+                             label="axes{}".format(i)) for i in range(len(variables))]
+
+        _, text = axes[0].set_thetagrids(angles, labels=variables)
+
+        for txt, angle in zip(text, angles):
+            if angle > -1 and angle < 181:
+                txt.set_rotation(angle - 90)
+            else:
+                txt.set_rotation(angle - 270)
+
+        for ax in axes[1:]:
+            ax.patch.set_visible(False)
+            ax.xaxis.set_visible(False)
+            ax.grid("off")
+
+        for i, ax in enumerate(axes):
+            grid = np.linspace(*ranges[i], num=n_ordinate_levels)
+            grid_label = [""]+["{:.0f}".format(x) for x in grid[1:-1]]
+            ax.set_rgrids(grid, labels=grid_label, angle=angles[i])
+            ax.set_ylim(*ranges[i])
+
+        self.angle = np.deg2rad(np.r_[angles, angles[0]])
+        self.ranges = ranges
+        self.ax = axes[0]
+
+    def plot(self, data, *args, **kw):
+        sdata = _scale_data(data, self.ranges)
+        self.ax.plot(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+
+    def fill(self, data, *args, **kw):
+        sdata = _scale_data(data, self.ranges)
+        self.ax.fill(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+
+    def legend(self, *args, **kw):
+        self.ax.legend(*args, **kw)
+
+    def title(self, title, *args, **kw):
+        self.ax.text(0.9, 1, title, transform=self.ax.transAxes, *args, **kw)
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+def graph_component_silhouette(n_clusters, lim_x, mat_size, sample_silhouette_values, clusters):
+    plt.rcParams["patch.force_edgecolor"] = True
+    plt.style.use('fivethirtyeight')
+    mpl.rc('patch', edgecolor='dimgray', linewidth=1)
+    # ____________________________
+    fig, ax1 = plt.subplots(1, 1)
+    fig.set_size_inches(8, 8)
+    ax1.set_xlim([lim_x[0], lim_x[1]])
+    ax1.set_ylim([0, mat_size + (n_clusters + 1) * 10])
+    y_lower = 10
+    for i in range(n_clusters):
+        # ___________________________________________________________________________________
+        # Aggregate the silhouette scores for samples belonging to cluster i, and sort them
+        ith_cluster_silhouette_values = sample_silhouette_values[clusters == i]
+        ith_cluster_silhouette_values.sort()
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+        cmap = cm.get_cmap("Spectral")
+        color = cmap(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.8)
+        # ____________________________________________________________________
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.03, y_lower + 0.5 * size_cluster_i, str(i), color='red', fontweight='bold',
+                 bbox=dict(facecolor='white', edgecolor='black', boxstyle='round, pad=0.3'))
+        # ______________________________________
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10
+
+
+def plot_learning_curve(estimator1, estimator2, estimator3, estimator4, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(
+        2, 2, figsize=(20, 14), sharey=True)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    # First Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator1, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax1.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax1.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax1.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax1.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax1.set_title("Logistic Regression Learning Curve", fontsize=14)
+    ax1.set_xlabel('Training size (m)')
+    ax1.set_ylabel('Score')
+    ax1.grid(True)
+    ax1.legend(loc="best")
+
+    # Second Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator2, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax2.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax2.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax2.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax2.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax2.set_title("Knears Neighbors Learning Curve", fontsize=14)
+    ax2.set_xlabel('Training size (m)')
+    ax2.set_ylabel('Score')
+    ax2.grid(True)
+    ax2.legend(loc="best")
+
+    # Third Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator3, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax3.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax3.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax3.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax3.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax3.set_title("Support Vector Classifier \n Learning Curve", fontsize=14)
+    ax3.set_xlabel('Training size (m)')
+    ax3.set_ylabel('Score')
+    ax3.grid(True)
+    ax3.legend(loc="best")
+
+    # Fourth Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator4, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax4.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax4.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax4.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax4.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax4.set_title("Decision Tree Classifier \n Learning Curve", fontsize=14)
+    ax4.set_xlabel('Training size (m)')
+    ax4.set_ylabel('Score')
+    ax4.grid(True)
+    ax4.legend(loc="best")
+    return plt
+
+
+def graph_roc_curve_multiple(log_fpr, log_tpr, knear_fpr, knear_tpr, svc_fpr, svc_tpr, tree_fpr, tree_tpr, y_train, log_reg_pred, knears_pred, svc_pred, tree_pred):
+    plt.figure(figsize=(16, 8))
+    plt.title('ROC Curve \n Top 4 Classifiers', fontsize=18)
+    plt.plot(log_fpr, log_tpr, label='Logistic Regression Classifier Score: {:.4f}'.format(
+        roc_auc_score(y_train, log_reg_pred)))
+    plt.plot(knear_fpr, knear_tpr, label='KNears Neighbors Classifier Score: {:.4f}'.format(
+        roc_auc_score(y_train, knears_pred)))
+    plt.plot(svc_fpr, svc_tpr, label='Support Vector Classifier Score: {:.4f}'.format(
+        roc_auc_score(y_train, svc_pred)))
+    plt.plot(tree_fpr, tree_tpr, label='Decision Tree Classifier Score: {:.4f}'.format(
+        roc_auc_score(y_train, tree_pred)))
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([-0.01, 1, 0, 1])
+    plt.xlabel('False Positive Rate', fontsize=16)
+    plt.ylabel('True Positive Rate', fontsize=16)
+    plt.annotate('Minimum ROC Score of 50% \n (This is the minimum score to get)', xy=(0.5, 0.5), xytext=(0.6, 0.3),
+                 arrowprops=dict(facecolor='#6E726D', shrink=0.05),
+                 )
+    plt.legend()
+
+
+def logistic_roc_curve(log_fpr, log_tpr):
+    plt.figure(figsize=(12, 8))
+    plt.title('Logistic Regression ROC Curve', fontsize=16)
+    plt.plot(log_fpr, log_tpr, 'b-', linewidth=2)
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlabel('False Positive Rate', fontsize=16)
+    plt.ylabel('True Positive Rate', fontsize=16)
+    plt.axis([-0.01, 1, 0, 1])
+
+
+# Create a confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=14)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
